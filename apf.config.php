f69c7bcb7f0eb14fe2392a5442fa7c444e740f69
Artnessde <timo@artness.de>
2009-12-23 14:53:20 +0100

    Added all needed files to the repo

---
+++ apf.config.php
@@ -0,0 +1,16 @@
+<?php
+
+	define( 'APF_JSMIN', 'JSMin/jsmin' );
+
+	$packs[2]	= array(
+					'name'		=>	'Pack1',
+					'filename'	=>	'Pack.001.js',
+					'files'		=>	array(
+										'Scripts/script1.js'	=> true,
+										'Scripts/script2.js'	=> true,
+										'Scripts/script3.js'	=> true,
+										'Scripts/script4.js'	=> true,
+										'Scripts/script5.js'	=> true
+									)
+					);
+?>
